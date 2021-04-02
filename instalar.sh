936e27820509055e2e9ba821be86ad95b0bc62b0
OEduardoEF <79720101+OEduardoEF@users.noreply.github.com>
2021-04-02 13:49:46 -0300

    Instalar.sh criado

---
+++ instalar.sh
@@ -0,0 +1,23 @@
+pacman -Syuu
+echo "Instalando o YAY"
+pacman -S --needed git base-devel
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
+
+echo "instalando aplicativos"
+
+yay -S nodejs npm yarn jdk-openjdk flatpak python datagrip intellij-idea-community-edition visual-studio-code-bin minecraft-launcher gnome-tweaks-git lutris uwufetch lolcat neofetch htop gimp kdenlive
+
+echo "instalando pacotes com flatpak"
+
+flatpak install flathub com.spotify.Client
+
+flatpak install flathub com.valvesoftware.Steam
+
+
+
+
