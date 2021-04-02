b2c79bb52c936ed791294cea1059b2f5b60a0b33
OEduardoEF <79720101+OEduardoEF@users.noreply.github.com>
2021-04-02 13:58:39 -0300

    Trocar Minecraft launcher por multimc

--- instalar.sh
+++ instalar.sh
@@ -10,7 +10,7 @@ makepkg -si
 
 echo "instalando aplicativos"
 
-yay -S nodejs npm yarn jdk-openjdk flatpak python datagrip intellij-idea-community-edition visual-studio-code-bin minecraft-launcher gnome-tweaks-git lutris uwufetch lolcat neofetch htop gimp firefox-nightly discord-canary kdenlive
+yay -S nodejs npm yarn jdk-openjdk flatpak python datagrip intellij-idea-community-edition visual-studio-code-bin multimc5 gnome-tweaks-git lutris uwufetch lolcat neofetch htop gimp firefox-nightly discord-canary kdenlive
 
 echo "instalando pacotes com flatpak"
 
