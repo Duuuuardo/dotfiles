b279f8407956f3eb995c27a4bd523b0bfe19d4d3
OEduardoEF <79720101+OEduardoEF@users.noreply.github.com>
2021-04-02 13:59:54 -0300

    Adicionado Minecraft bedrock launcher

--- instalar.sh
+++ instalar.sh
@@ -1,23 +1,15 @@
 pacman -Syuu
 echo "Instalando o YAY"
 pacman -S --needed git base-devel
-
 git clone https://aur.archlinux.org/yay.git
-
 cd yay
-
 makepkg -si
-
 echo "instalando aplicativos"
-
 yay -S nodejs npm yarn jdk-openjdk flatpak python datagrip intellij-idea-community-edition visual-studio-code-bin multimc5 gnome-tweaks-git lutris uwufetch lolcat neofetch htop gimp firefox-nightly discord-canary kdenlive
-
 echo "instalando pacotes com flatpak"
-
 flatpak install flathub com.spotify.Client
-
 flatpak install flathub com.valvesoftware.Steam
-
+flatpak install flathub io.mrarm.mcpelauncher
 
 
 
